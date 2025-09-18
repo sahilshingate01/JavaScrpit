a1a69f4d81233d449ab2dd48f9adef752df19f5b
sahilshingate01 <sahilshingate0@gmail.com>
2025-09-18 11:47:35 +0530

    file name chnage and new file and code added

---
+++ Day_1/Data_types.js
@@ -0,0 +1,6 @@
+let str = "Hello";
+let num = 42;
+let isStudent = true;
+let nothing = null;
+let notDefined;
+console.log(typeof str, typeof num, typeof isStudent, typeof nothing, typeof notDefined);
