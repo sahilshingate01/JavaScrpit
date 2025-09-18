3e261fce88346f5bb9414bfc892332cf002805ed
sahilshingate01 <sahilshingate0@gmail.com>
2025-09-18 11:41:18 +0530

    Basic file and code added

---
+++ Day_1/Basic.js
@@ -0,0 +1,16 @@
+// Variables
+let name = "Sahil";
+let age = 20;
+let isStudent = true;
+
+// Data types
+console.log(typeof name);   // string
+console.log(typeof age);    // number
+console.log(typeof isStudent); // boolean
+
+// Operators
+let sum = 10 + 5;
+let product = 4 * 3;
+console.log("Sum:", sum);
+console.log("Product:", product);
+
