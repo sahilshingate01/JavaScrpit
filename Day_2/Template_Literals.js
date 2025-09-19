ecf271fe1f42739e53b93ebfe641e00d318834a3
sahilshingate01 <sahilshingate0@gmail.com>
2025-09-19 23:18:57 +0530

    day 2 file added and template file and code added

---
+++ Day_2/Template_Literals.js
@@ -0,0 +1,14 @@
+// We are declaring a variable called 'name' using 'let'.
+// 'let' is used to store values that can change later in the program.
+// Here, we are storing the string "Sahil" inside the variable 'name'.
+let name = "Sahil";
+
+// console.log() is a built-in function in JavaScript that prints output to the console.
+// The console is useful for testing and debugging your code.
+// Inside console.log(), we are using a "template literal" (the text inside backticks ``).
+// Template literals let us easily insert variables or expressions into strings using ${ }.
+// Here, ${name} will be replaced with the actual value stored in 'name', which is "Sahil".
+console.log(`Hello, ${name}! Welcome to JS.`);
+
+// Final Output on the console will be:
+// Hello, Sahil! Welcome to JS.
